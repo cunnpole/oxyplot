3cc024a4f16168896c7704b5f55373e964d8c433
cunnpole <cunnpole@hotmail.com>
2018-01-28 23:58:07 +0000

    Fixed project designer issues

--- Source/Examples/WindowsForms/WindowsFormsDemo/Properties/Resources.Designer.cs
+++ Source/Examples/WindowsForms/WindowsFormsDemo/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WindowsFormsDemo.Properties {
+namespace APITest.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace WindowsFormsDemo.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WindowsFormsDemo.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("APITest.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
